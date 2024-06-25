1ff3cd1395e6826ea9c32c2e593a0036a939efc9
Jose Villeda <jose@exeboard.com>
2024-06-25 00:32:03 -0600

    Test

--- config/default.ts
+++ config/default.ts
@@ -1,39 +1,25 @@
+import {
+  DB_URI, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL, ORIGIN, PORT,
+  PRIVATE_KEY,
+  PUBLIC_KEY,
+} from 'src/settings';
+
 export default {
   server: {
-    port: 1234,
+    port: PORT,
   },
   db: {
-    uri: 'mongodb://localhost:27017/oauth-google-poc',
+    uri: DB_URI,
   },
   googleOAuth: {
-    clientId:
-      '535608196914-435ov2uqphk7gsskl3lvp1f20a2d0nvn.apps.googleusercontent.com',
-    clientSecret: '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',
-    redirectUrl: 'http://localhost:1234/api/sessions/oauth/google',
+    clientId: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    redirectUrl: GOOGLE_REDIRECT_URL,
   },
   saltWorkFactor: 10,
   accessTokenTtl: '15m',
   refreshTokenTtl: '1y',
-  privateKey: `-----BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQCXzZerpx9qdaelwt1U7NCpWXQKkm1OW4ohDF/7g01xDtYf8Nox
-9wzhhVQrFD+G4eaJoWxIhJYQTgT4ijMlpjXs07McwktcMX49h6Eoo6ZddOMl380U
-pivkaO+h80miG4JCFAM0G0pUoeNT8h6L9zHqr/yEoBRd3RAsqxeCKwKrswIDAQAB
-AoGASdYpcMewQzMJIVpgF7+8WrL0+3NvkU57KEoBoa+jClviENUw/F6jOEqGvxFx
-OOVGIimPJtK+vx2D03Q9HpHy5gfG3Q0hXq8RABfzOkgKOQJYEb/CtkadRo9sqPpw
-PtXaFrwYNPevqzUydk5Y4imQN0yycmKPirgWP2GGZj8nAxECQQDMmWI6BkE+8p0H
-PszrkpDlAo3p/4oMW4XgoxBbIuHMuzYZi6d6FUvcOjGI88ktTBSMIhwjF5K/lIBU
-RXcKjzKJAkEAvfCtHN6oLs22MXp1AZBTSIfLp/8C4nLfW6VMboR+Sw9R5MatTuTI
-W4seeBxn1/DU88IhU579maYMX8TFMITNWwJATAPMz9wVD6saFmAJyJhKxxsc2Mx9
-YLv8UIqlAAdEt0jy/6i4T45sAeWJE+XnX7H9jFb79znB5vXoe+bXJJAxaQJBAK9P
-WeigleiFFBl85kvoMwZp2A4Z8EakMgX7fp0vCwYqgLLAsat0vHzJ3fiMd0g3T8Yg
-svPkdjt/qEIocryChqsCQGnInFDk9fC5C8G1Ez2N/p0WztoPDNKfYVDGZ8IRMjEy
-rPeH03CIhIoh6QPPg02GKC3VumQxgwOe2wkQ2OgHYLE=
------END RSA PRIVATE KEY-----`,
-  publicKey: `-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXzZerpx9qdaelwt1U7NCpWXQK
-km1OW4ohDF/7g01xDtYf8Nox9wzhhVQrFD+G4eaJoWxIhJYQTgT4ijMlpjXs07Mc
-wktcMX49h6Eoo6ZddOMl380UpivkaO+h80miG4JCFAM0G0pUoeNT8h6L9zHqr/yE
-oBRd3RAsqxeCKwKrswIDAQAB
------END PUBLIC KEY-----`,
-  origin: 'http://localhost:3000',
+  privateKey: PRIVATE_KEY,
+  publicKey: PUBLIC_KEY,
+  origin: ORIGIN,
 };
---
+++ src/settings.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+
+export const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+
+export const PUBLIC_KEY = process.env.PUBLIC_KEY || '';
+
+export const PORT = process.env.PORT || 1234;
+
+export const DB_URI = process.env.DB_URI || '';
+
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '';
+
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '';
+
+export const GOOGLE_REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL || '';
+
+export const ORIGIN = process.env.ORIGIN || '';
